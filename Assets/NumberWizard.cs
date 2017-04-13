374ec44cd6f4c6b7ddd965065f90e588b9bb5fe2
Unknown <k.ballagher@gmail.com>
2017-04-13 15:41:05 +0100

    Imported Number Wizard Console Code
    Imported old code + adapted for button presses.
    Workable game.

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+
+    int maxGuessesAllowed = 10;
+
+    public Text text;
+
+	void Start () {
+		StartGame ();	
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+	
+		max = max + 1;
+		
+	}
+	
+	void NextGuess () {
+		guess = (max + min) / 2;
+        text.text = "Is your number " + guess.ToString() + "?";
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+        if (maxGuessesAllowed <= 0){
+            Application.LoadLevel("Win");
+        }
+		
+	}
+	
+    public void GuessHigher() {
+        min = guess;
+        NextGuess();
+
+    }
+
+    public void GuessLower() {
+        max = guess;
+        NextGuess();
+}
+
+   
+    
+}
